2ebfddc1c35d24549d2726cc67d27ea4c39d6460
Matt <matthewbrion@me.com>
2026-06-26 12:08:44 -0400

    styled and finished

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const API_BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2605-ftb-ct-web-pt-mbs";
 
 const state = {
     parties: [],
@@ -12,7 +12,7 @@ const getParties = async() => {
         state.parties = data.data;
         render()
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
@@ -23,10 +23,31 @@ const getPartyById = async(id) => {
         state.selectedParty = data.data
         render()
     } catch (error) {
+        console.log(error)
+    }
+}
 
+const partySelected = () => {
+    if (state.selectedParty == null) {
+        return `<div class="party-detail"><h3>Select a party from the list to see more info.</h3></div>?`
+    } else {
+        const formattedDate = new Date(state.selectedParty.date).toISOString().split('T')[0]
+        return `<div class="party-detail">
+        <h3>${state.selectedParty.name} ${state.selectedParty.id}</h3>
+        <p>${formattedDate}</p>
+        <p>${state.selectedParty.location}</p>
+        <p>${state.selectedParty.description}</p>
+        </div>`
     }
 }
 
 const showParties = () => {
-    return state.parties.map((party) => `<li>${party.name}</li>`).join("")
+    return `<div class="party-list"><ul>${state.parties.map((party) => `<li class="${party.id === state.selectedParty?.id ? 'selected' : ''}" onclick=" getPartyById(${party.id})">${party.name}</li>`).join("")}</ul></div>`
 }
+
+const render = () => {
+    const partiesUi = document.getElementById('app')
+    partiesUi.innerHTML = showParties() + partySelected()
+}
+
+getParties()
